daa4fa5825f76ac6ac1084458dd3ea7401b4eb06
Khairunshaik <shaikkhairunnisa900@gmail.com>
2023-08-06 23:12:28 +0530

    my-youtube-app

---
+++ src/data.js
@@ -0,0 +1,249 @@
+// src/data.js
+const videos = [
+    {
+      id: 1,
+      title: "Laila manju",
+      views: 12740,
+      likes: 503840,
+      channel: {
+        name: "Channel 1",
+        logo: "https://tse3.mm.bing.net/th?id=OIP.aCjeOqVIXWi5-RJgWiE24gHaFj&pid=Api&P=0&h=180",
+      },
+      thumbnail: "https://tse2.mm.bing.net/th?id=OIP.CGc_mxwjQbrNCLFT36UgbwHaEK&pid=Api&P=0&h=180",
+    },
+    {
+      id: 2,
+      title: "Dil se",
+      views: 20347,
+      likes: 33240,
+      channel: {
+        name: "Channel 2",
+        logo: "https://tse3.mm.bing.net/th?id=OIP.lpNKg1izU0-bLkk8bKy2IAHaGx&pid=Api&P=0&h=180",
+      },
+      thumbnail: "https://3.bp.blogspot.com/-r97QpLuhr24/TmnhecylPFI/AAAAAAAAAYA/0kbghBIvB44/s1600/Slide10.JPG",
+    },
+    {
+      id: 3,
+      title: "MS Doni",
+      views: 45000,
+      likes: 56000,
+      channel: {
+        name: "Channel 3",
+        logo: "https://bcassetcdn.com/public/blog/wp-content/uploads/2019/10/18094207/movie-reviews.png",
+      },
+      thumbnail: "http://straightfromamovie.com/wp-content/uploads/2016/10/M.S.-Dhoni.jpg",
+    },
+    {
+      id: 4,
+      title: "Gadar 2",
+      views: 65000,
+      likes: 76000,
+      channel: {
+        name: "Channel 4",
+        logo: "https://tse4.mm.bing.net/th?id=OIP.8Zh-B6fmNXff-_LO2FSXXwHaHc&pid=Api&P=0&h=180",
+      },
+      thumbnail: "https://img.theweek.in/content/dam/week/news/entertainment/images/2023/6/12/gadar-2.jpg",
+    },
+    {
+      id: 5,
+      title: "A Gentlemen",
+      views: 10000,
+      likes: 76000,
+      channel: {
+        name: "Channel 5",
+        logo: " https://tse3.mm.bing.net/th?id=OIP.__01awj_4BnXF8mlKELplQHaHa&pid=Api&P=0&h=180",
+      },
+      thumbnail: "https://static.india.com/wp-content/uploads/2017/08/gentleman-3.jpg",
+    },
+    {
+      id: 6,
+      title: "",
+      views: 69000,
+      likes: 96000,
+      channel: {
+        name: "Channel 6",
+        logo: "https://tse2.mm.bing.net/th?id=OIP.p_JCDd9FqHzi1RlI56r-TAHaFj&pid=Api&P=0&h=180 ",
+      },
+      thumbnail: "https://wallpapercave.com/wp/wp6989227.jpg",
+    },
+    {
+      id: 7,
+      title: "Agent Vinod",
+      views: 610000,
+      likes: 767000,
+      channel: {
+        name: "Channel 7",
+        logo: "https://tse3.mm.bing.net/th?id=OIP.OTKb506ecMGEJl8_NWTymwHaEK&pid=Api&P=0&h=180 ",
+      },
+      thumbnail: "https://tse4.mm.bing.net/th?id=OIP.RtY60KeLdi9XFyF71gZxhQHaDt&pid=Api&P=0&h=180",
+    },
+    {
+      id: 8,
+      title: "Roohi",
+      views: 745000,
+      likes: 906000,
+      channel: {
+        name: "Channel 8",
+        logo: "https://logodix.com/logo/736519.jpg ",
+      },
+      thumbnail: "https://hityaflopmovieworld.com/wp-content/uploads/2021/02/roohi-official-trailer.jpg",
+    },
+    {
+      id: 9,
+      title: "Heropanti",
+      views: 65000,
+      likes: 76000,
+      channel: {
+        name: "Channel 9",
+        logo: " http://clipart-library.com/images_k/youtube-logo-transparent-png/youtube-logo-transparent-png-25.png",
+      },
+      thumbnail: "http://www.moviehdwallpapers.com/wp-content/uploads/Heropanti-Movie-Poster1.jpg",
+    },
+    {
+      id: 10,
+      title: "Barfi",
+      views: 659300,
+      likes: 906000,
+      channel: {
+        name: "Channel 10",
+        logo: "https://technicoz.com/wp-content/uploads/2020/06/Image11-scaled.jpg ",
+      },
+      thumbnail: "https://tse4.mm.bing.net/th?id=OIP.Zr2jr8et_xfuFXvOpH2JbAAAAA&pid=Api&P=0&h=180",
+    },
+    {
+      id: 11,
+      title: "RHTOM",
+      views: 875000,
+      likes: 906000,
+      channel: {
+        name: "Channel 11",
+        logo: "https://i.ytimg.com/vi/X5Fe5SCKC7U/maxres2.jpg?sqp=-oaymwEoCIAKENAF8quKqQMcGADwAQH4Ac4FgAKACooCDAgAEAEYZSBlKGUwDw==&rs=AOn4CLBKW4KfgzERSiDHzaj7tE0DP2Wmqw ",
+      },
+      thumbnail: "https://tse1.mm.bing.net/th?id=OIP._kOcXlKaiF2QCMzt9HdLggHaE_&pid=Api&P=0&h=180",
+    },
+    {
+      id: 12,
+      title: "Ishq",
+      views: 767400,
+      likes: 253000,
+      channel: {
+        name: "Channel 12",
+        logo: " https://cdn.dribbble.com/users/640044/screenshots/3785937/spoon.png",
+      },
+      thumbnail: "https://4.bp.blogspot.com/-aeMcto8J8OI/WDhy3qOz5HI/AAAAAAAAAB4/yIaRnuFlYdE88xk7C_AQWZebiAJNQl_GACLcB/s1600/Ishq%2BFull%2BMovie.jpg",
+    },
+    {
+      id: 13,
+      title: "Phone Bhoot",
+      views: 6423000,
+      likes: 5372000,
+      channel: {
+        name: "Channel 13",
+        logo: "https://tse2.mm.bing.net/th?id=OIP.JOMLC5BBuKhpLOsQrW8AIAHaHC&pid=Api&P=0&h=180 ",
+      },
+      thumbnail: "https://images.ottplay.com/photos/Phone_Bhoot_2022_movie_photo_1_573.jpeg",
+    },
+    {
+      id: 13,
+      title: "Ek Villan",
+      views: 657890,
+      likes: 763563,
+      channel: {
+        name: "Channel 13",
+        logo: "https://2.bp.blogspot.com/-t_ccj1R9UOI/Wpl2TcXykBI/AAAAAAAABWA/7nEiJLiGyBIbkLMX-BqbFuOgAtaSFgs7QCLcBGAs/s1600/MH3.png ",
+      },
+      thumbnail: "https://images-na.ssl-images-amazon.com/images/I/613EwJnu-OL.jpg",
+    },
+    {
+      id: 14,
+      title: "Thank God",
+      views: 365764,
+      likes: 687685,
+      channel: {
+        name: "Channel 14",
+        logo: "https://cdn5.f-cdn.com/contestentries/861199/18918443/57f8db6c76afa_thumb900.jpg",
+      },
+      thumbnail: "https://s.enavabharat.com/wp-content/uploads/2022/09/Thank-God-trailer-.jpg",
+    },
+    {
+      id: 15,
+      title: "Liger",
+      views: 3565732,
+      likes: 737574,
+      channel: {
+        name: "Channel 15",
+        logo: " https://tse3.mm.bing.net/th?id=OIP.ER2CM4e8FKgXw0FZ3qtU8gHaC5&pid=Api&P=0&h=180",
+      },
+      thumbnail: "https://www.tollywood.net/wp-content/uploads/2022/08/Liger-Movie-Review-and-Rating.jpg",
+    },
+    {
+      id: 16,
+      title: "Brahmastra",
+      views: 983294,
+      likes: 27374,
+      channel: {
+        name: "Channel 16",
+        logo: "http://clipart-library.com/new_gallery/6-60650_logo-youtube-png-icon-youtube-logo-png.png",
+      },
+      thumbnail: "https://m.media-amazon.com/images/M/MV5BNzk2MDMxYTctZjdlMy00NWU4LThjZjgtMjdjODBlZDk0NDNjXkEyXkFqcGdeQXRyYW5zY29kZS13b3JrZmxvdw@@._V1_.jpg",
+    },
+    {
+      id: 17,
+      title: "Bhediya",
+      views: 653746,
+      likes: 7348950,
+      channel: {
+        name: "Channel 17",
+        logo: "https://tse4.mm.bing.net/th?id=OIP.YmG4PJ5OcFl-AfMylZA2sQHaHa&pid=Api&P=0&h=180 ",
+      },
+      thumbnail: "https://i.ytimg.com/vi/XRRtM3kML-E/maxresdefault.jpg",
+    },
+    {
+      id: 18,
+      title: "3 Idiots",
+      views: 6532864,
+      likes: 90000,
+      channel: {
+        name: "Channel 18",
+        logo: "https://tse2.mm.bing.net/th?id=OIP.fUsnc7ytdEiQe28AhWKlWwHaEK&pid=Api&P=0&h=180 ",
+      },
+      thumbnail: "https://i.zoomtventertainment.com/story/Untitled_design_-_2020-05-01T135137.466.jpg",
+    },
+    {
+      id: 19,
+      title: "Pathaan",
+      views: 284844,
+      likes: 4977584,
+      channel: {
+        name: "Channel 19",
+        logo: "https://i.pinimg.com/originals/4e/84/06/4e8406bc354cd59eeebcdd6ec94b9645.jpg ",
+      },
+      thumbnail: "https://tse1.mm.bing.net/th?id=OIP.VvHbu-piflN2ctdfGRrwCgHaD5&pid=Api&P=0&h=180",
+    },
+    {
+      id: 20,
+      title: "Shershaah",
+      views: 84364632,
+      likes: 26353274,
+      channel: {
+        name: "Channel 20",
+        logo: "https://www.underconsideration.com/brandnew/archives/youtube_logo.png ",
+      },
+      thumbnail: "https://tse1.mm.bing.net/th?id=OIP.7u6ov7SwXvqYWQmBmF-TuAHaD2&pid=Api&P=0&h=180",
+    },
+    {
+      id: 21,
+      title: "singham",
+      views: 94364632,
+      likes: 16353274,
+      channel: {
+        name: "Channel 21",
+        logo: "https://preview.redd.it/j1508hi093l61.jpg?auto=webp&s=81fc5c992be7cead991153135701595a6c66bf7e ",
+      },
+      thumbnail: "https://tse4.mm.bing.net/th?id=OIP.znb6N1KZuygz29r1O1jlRwHaEo&pid=Api&P=0&h=180",
+    },
+
+  ];
+  
+  export default videos;
+  
